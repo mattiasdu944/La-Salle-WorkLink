269b17798f421efbf97034e8a41b8e2d66f72092
Mattias <duartemattias3@gmail.com>
2023-04-14 22:56:52 -0400

    Adding ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
+import { ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { lightTheme } from '../theme/index';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <ThemeProvider theme={ lightTheme }>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
