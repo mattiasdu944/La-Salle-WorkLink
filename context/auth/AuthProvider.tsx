99ca605b2f14a7f7b7abcb8c15391b9a9e2ead37
Mattias <duartemattias3@gmail.com>
2023-04-28 16:41:43 -0400

    update api folder

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 import { AuthContext, authReducer } from './';
 
 
-import { worklinkApi } from '../../api';
+import { worklinkApi } from '../../worklinkApi';
 import { IUser } from '../../interfaces';
 
 export interface AuthState {
