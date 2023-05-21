cf85603ab80b66873fab7baae5f946834b4487d8
Mattias <duartemattias3@gmail.com>
2023-05-21 17:33:52 -0400

    validate forms helpers

--- helpers/validate-forms.ts
+++ helpers/validate-forms.ts
@@ -1,4 +1,4 @@
-interface ValidateRegisterStudent {
+export interface ValidateRegisterStudent {
     name: string, 
     email: string, 
     password: string, 
@@ -22,7 +22,7 @@ export const validateRegisterStudent: (registerForm: ValidateRegisterStudent, se
         return;
     }
 
-    if( email.trim() === '' || email.trim().length < 4 ){
+    if( email.trim() === '' || email.trim().length < 4 || !email.includes('est.ulasalle.edu.bo') ){
         setError('Ingrese una direccion de correo valido')
         setIsLoading(false);
         return;
@@ -49,6 +49,69 @@ export const validateRegisterStudent: (registerForm: ValidateRegisterStudent, se
     setError(undefined)
 };
 
-export const validateRegisterCompany = () => {}; 
+export interface ValidateRegisterCompany {
+    name: string, 
+    email: string, 
+    password: string, 
+    username: string, 
+    passwordConfirmation:string
+}
 
-export const validateLogin = () => {}; 
+export const validateRegisterCompany : (registerForm: ValidateRegisterCompany, setError: any, setIsLoading: any) => void = ( registerForm, setError, setIsLoading ) => {
+
+    const { email, name, password, passwordConfirmation, username } = registerForm;
+    
+    if( name.trim() == '' || name.trim().length < 4){
+        setError('Ingrese un nombre valido');
+        setIsLoading(false);
+        return;
+    }
+
+
+    if( email.trim() === '' || email.trim().length < 4 ){
+        setError('Ingrese una direccion de correo valido')
+        setIsLoading(false);
+        return;
+    }
+
+    if( username.trim().length < 4){
+        setError('Nombre de usuario muy corto')
+        setIsLoading(false);
+        return;
+    }
+    
+    if( password.length < 8 ){
+        setError('La contraseña debe tener minimo 8 caracteres')
+        setIsLoading(false);
+        return;
+    }
+
+    if( password.trim() !== passwordConfirmation.trim() ){
+        setError('Las contraseñas no coinciden')
+        setIsLoading(false);
+        return;
+    }
+
+    setError(undefined)
+}; 
+
+
+export interface ValidateLogin {
+    email: string, 
+    password: string, 
+}
+export const validateLogin: (loginForm: ValidateLogin, setError: any, setIsLoading: any ) => void = (loginForm, setError, setIsLoading) => {
+    const { email, password } = loginForm;
+    
+    if( email.trim() === '' ){
+        setError('Ingrese un correo valido')
+        setIsLoading(false);
+        return;
+    }
+    if( password.length < 8 ){
+        setError('La contraseña debe tener minimo 8 caracteres')
+        setIsLoading(false);
+        return;
+    }
+    setError( undefined );
+}; 
