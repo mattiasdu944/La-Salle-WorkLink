95b59c030c346103a588b53a8d9e03815a40755c
Mattias <duartemattias3@gmail.com>
2023-05-24 09:30:21 -0400

    login en base el rol

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { db } from '@/database';
-import { User } from '@/models';
+import { User, Company } from '@/models';
 
 
 import bcrypt from 'bcrypt';
@@ -50,43 +50,75 @@ const loginUser = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
     const user = await User.findOne({ email });
     await db.disconnect();
 
+    if( user ){
+        // Verificar la contraseña
+        const validPassword = bcrypt.compareSync( password, user.password! );
+        if ( !validPassword ) {
+            return res.status(500).json({
+                message: 'Credenciales incorrectas'
+            })
+        }
+
+        const token = await generarJWT(user._id)
+        
+        await db.connect();
+        user.token = token;
+        await user.save();
+        await db.disconnect();
+
+        const { name, _id, username, image } = user;
 
 
-    if ( !user ) {
-        return res.status(400).json({
-            message:'Credenciales incorrectas'
+        return res.status(200).json({
+            user: {
+                _id,
+                username,
+                email, 
+                role:'student', 
+                name,
+                image
+            },
+            token
         })
     }
 
-    // Verificar la contraseña
-    const validPassword = bcrypt.compareSync( password, user.password! );
-    if ( !validPassword ) {
-        return res.status(500).json({
-            message: 'Credenciales incorrectas'
-        })
-    }
-
-    const token = await generarJWT(user._id)
-    
     await db.connect();
-    user.token = token;
-    await user.save();
+    const company = await Company.findOne({ email });
     await db.disconnect();
 
-    const { role, name, _id, username, image } = user;
+    if( company ){
+        // Verificar la contraseña
+        const validPassword = bcrypt.compareSync( password, company.password! );
+        if ( !validPassword ) {
+            return res.status(500).json({
+                message: 'Credenciales incorrectas'
+            })
+        }
+
+        const token = await generarJWT(company._id)
+        
+        await db.connect();
+        company.token = token;
+        await company.save();
+        await db.disconnect();
+
+        const { name, _id, username, image } = company;
 
 
-    return res.status(200).json({
-        user: {
-            _id,
-            username,
-            email, 
-            role, 
-            name,
-            image
-        },
-        token
+        return res.status(200).json({
+            user: {
+                _id,
+                username,
+                email, 
+                role:'company', 
+                name,
+                image
+            },
+            token
+        })
+    }
+    
+    return res.status(400).json({
+        message:'Credenciales incorrectas'
     })
-
-
 }
